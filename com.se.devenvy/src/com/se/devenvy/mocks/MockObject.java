bef1fcc5261e0bf64f2787b6e0d952b5f7ff5347
Gary Buyn <simplegsb@gmail.com>
2010-08-17 17:17:54 +0000

    Added capabilities for checking order of calls with parameters. Added capabilities for comparing enum parameters.

--- com.se.devenvy/src/com/se/devenvy/mocks/MockObject.java
+++ com.se.devenvy/src/com/se/devenvy/mocks/MockObject.java
@@ -12,194 +12,271 @@ import java.util.ArrayList;
  */
 public abstract class MockObject
 {
-	/**
-	 * <p>
-	 * A method call that hass been made to this <code>MockObject</code>.
-	 * </p>
-	 * 
-	 * @author simple
-	 */
-	protected class MethodCall
-	{
-		public String name;
-		public Object[] parameters;
-	}
+    /**
+     * <p>
+     * A method call that hass been made to this <code>MockObject</code>.
+     * </p>
+     * 
+     * @author simple
+     */
+    protected class MethodCall
+    {
+        public String name;
+        public Object[] parameters;
+    }
 
-	/**
-	 * <p>
-	 * The method calls that have been made on this <code>MockObject</code>.
-	 * </p>
-	 */
-	private ArrayList<MethodCall> methodCalls;
+    /**
+     * <p>
+     * The method calls that have been made on this <code>MockObject</code>.
+     * </p>
+     */
+    private ArrayList<MethodCall> methodCalls;
 
-	/**
-	 * <p>
-	 * Creates an instance of <code>MockObject</code>.
-	 * </p>
-	 */
-	public MockObject()
-	{
-		methodCalls = new ArrayList<MethodCall>();
-	}
+    /**
+     * <p>
+     * Creates an instance of <code>MockObject</code>.
+     * </p>
+     */
+    public MockObject()
+    {
+        methodCalls = new ArrayList<MethodCall>();
+    }
 
-	/**
-	 * <p>
-	 * Adds a method call that has been made on this <code>MockObject</code>.
-	 * </p>
-	 * 
-	 * @param name The name of the method that has been called on this <code>MockObject</code>.
-	 * @param parameters The parameters passed to this <code>MockObject</code> when the method was called, or <code>null</code>
-	 * if no parameters were given.
-	 */
-	protected void addMethodCall(final String name, final Object[] parameters)
-	{
-		MethodCall methodCall = new MethodCall();
+    /**
+     * <p>
+     * Adds a method call that has been made on this <code>MockObject</code>.
+     * </p>
+     * 
+     * @param name
+     *            The name of the method that has been called on this <code>MockObject</code>.
+     * @param parameters
+     *            The parameters passed to this <code>MockObject</code> when the method was called, or <code>null</code> if no
+     *            parameters were given.
+     */
+    protected void addMethodCall(final String name, final Object[] parameters)
+    {
+        MethodCall methodCall = new MethodCall();
 
-		methodCall.name = name;
-		methodCall.parameters = parameters;
+        methodCall.name = name;
+        methodCall.parameters = parameters;
 
-		methodCalls.add(methodCall);
-	}
+        methodCalls.add(methodCall);
+    }
 
-	/**
-	 * <p>
-	 * Checks the equality of the two given sets of parameters.
-	 * </p>
-	 * 
-	 * @param parameterSetA The first set of parameters to compare.
-	 * @param parameterSetB The second set of parameters to compare.
-	 * 
-	 * @return True if the sets of parameters are equal, false otherwise.
-	 */
-	protected boolean parametersEqual(Object[] parameterSetA, Object[] parameterSetB)
-	{
-		if (parameterSetA == null && parameterSetB == null)
-		{
-			return (true);
-		}
+    /**
+     * <p>
+     * Retrieves the number of calls made to the given method with the given parameters on this <code>MockObject</code>.
+     * </p>
+     * 
+     * @param name
+     *            The name of the method to check the number of method calls for.
+     * @param parameters
+     *            The parameters to check the number of method calls for.
+     * 
+     * @return The number of calls made to the given method with the given parameters on this <code>MockObject</code>.
+     */
+    public int getMethodCallCount(String name, Object[] parameters)
+    {
+        int methodCallCount = 0;
 
-		if (parameterSetA == null || parameterSetB == null)
-		{
-			return (false);
-		}
+        for (MethodCall methodCall : methodCalls)
+        {
+            if (methodCall.name.equals(name) && parametersEqual(methodCall.parameters, parameters))
+            {
+                methodCallCount++;
+            }
+        }
 
-		if (parameterSetA.length != parameterSetB.length)
-		{
-			return (false);
-		}
+        return (methodCallCount);
+    }
 
-		for (int index = 0; index < parameterSetA.length; index++)
-		{
-			if (!parameterSetA[index].equals(parameterSetB[index]))
-			{
-				return (false);
-			}
-		}
+    /**
+     * <p>
+     * Retrieves the number of calls made to the given method on this <code>MockObject</code>.
+     * </p>
+     * 
+     * @param name
+     *            The name of the method to check the number of method calls for.
+     * 
+     * @return The number of calls made to the given method on this <code>MockObject</code>.
+     */
+    public int getMethodCallCountIgnoreParams(String name)
+    {
+        int methodCallCount = 0;
 
-		return (true);
-	}
+        for (MethodCall methodCall : methodCalls)
+        {
+            if (methodCall.name.equals(name))
+            {
+                methodCallCount++;
+            }
+        }
 
-	/**
-	 * <p>
-	 * Retrieves the number of calls made to the given method with the given parameters on this <code>MockObject</code>.
-	 * </p>
-	 * 
-	 * @param name The name of the method to check the number of method calls for.
-	 * @param parameters The parameters to check the number of method calls for.
-	 * 
-	 * @return The number of calls made to the given method with the given parameters on this <code>MockObject</code>.
-	 */
-	public int getMethodCallCount(String name, Object[] parameters)
-	{
-		int methodCallCount = 0;
+        return (methodCallCount);
+    }
 
-		for (MethodCall methodCall : methodCalls)
-		{
-			if (methodCall.name.equals(name) && parametersEqual(methodCall.parameters, parameters))
-			{
-				methodCallCount++;
-			}
-		}
+    /**
+     * <p>
+     * Determines whether the nth call to one method with the given parameters was made before the mth call to another method with
+     * the given parameters.
+     * </p>
+     * 
+     * @param beforeCallIndex
+     *            The index of the call to the method that is expected to have come before (the nth call). The first call is index
+     *            0.
+     * @param beforeMethodName
+     *            The method that is expected to have been called before.
+     * @param beforeMethodParameters
+     *            The parameters of the method that is expected to have been called before.
+     * @param afterCallIndex
+     *            The index of the call to the method that is expected to have come after (the mth call). The first call is index
+     *            0.
+     * @param afterMethodName
+     *            The method that is expected to have been called after.
+     * @param afterMethodParameters
+     *            The parameters of the method that is expected to have been called after.
+     * 
+     * @return True if the <code>n</code>th call to one method was made before the <code>m</code>th call to another.
+     */
+    public boolean methodCallOrderCheck(int beforeCallIndex, String beforeMethodName, Object[] beforeMethodParameters,
+            int afterCallIndex, String afterMethodName, Object[] afterMethodParameters)
+    {
+        int beforeCalls = 0;
+        int afterCalls = 0;
 
-		return (methodCallCount);
-	}
+        for (MethodCall methodCall : methodCalls)
+        {
+            if (methodCall.name.equals(beforeMethodName) && parametersEqual(methodCall.parameters, beforeMethodParameters))
+            {
+                if (beforeCalls == beforeCallIndex)
+                {
+                    return (true);
+                }
 
-	/**
-	 * <p>
-	 * Retrieves the number of calls made to the given method on this <code>MockObject</code>.
-	 * </p>
-	 * 
-	 * @param name The name of the method to check the number of method calls for.
-	 * 
-	 * @return The number of calls made to the given method on this <code>MockObject</code>.
-	 */
-	public int getMethodCallCountIgnoreParams(String name)
-	{
-		int methodCallCount = 0;
+                beforeCalls++;
+            }
+            else if (methodCall.name.equals(afterMethodName) && parametersEqual(methodCall.parameters, afterMethodParameters))
+            {
+                if (afterCalls == afterCallIndex)
+                {
+                    return (false);
+                }
 
-		for (MethodCall methodCall : methodCalls)
-		{
-			if (methodCall.name.equals(name))
-			{
-				methodCallCount++;
-			}
-		}
+                afterCalls++;
+            }
+        }
 
-		return (methodCallCount);
-	}
+        return (false);
+    }
 
-	/**
-	 * <p>
-	 * Determines whether the nth call to one method was made before the mth call to another. method.
-	 * </p>
-	 * 
-	 * @param beforeCallIndex The index of the call to the method that is expected to have come before (the nth call). The first
-	 * call is index 0.
-	 * @param beforeMethodName The method that is expected to have been called before.
-	 * @param afterCallIndex The index of the call to the method that is expected to have come after (the mth call). The first
-	 * call is index 0.
-	 * @param afterMethodName The method that is expected to have been called after.
-	 * 
-	 * @return True if the <code>n</code>th call to one method was made before the <code>m</code>th call to another.
-	 */
-	public boolean methodCallOrderCheckIgnoreParams(int beforeCallIndex, String beforeMethodName, int afterCallIndex,
-			String afterMethodName)
-	{
-		int beforeCalls = 0;
-		int afterCalls = 0;
+    /**
+     * <p>
+     * Determines whether the nth call to one method was made before the mth call to another method.
+     * </p>
+     * 
+     * @param beforeCallIndex
+     *            The index of the call to the method that is expected to have come before (the nth call). The first call is index
+     *            0.
+     * @param beforeMethodName
+     *            The method that is expected to have been called before.
+     * @param afterCallIndex
+     *            The index of the call to the method that is expected to have come after (the mth call). The first call is index
+     *            0.
+     * @param afterMethodName
+     *            The method that is expected to have been called after.
+     * 
+     * @return True if the <code>n</code>th call to one method was made before the <code>m</code>th call to another.
+     */
+    public boolean methodCallOrderCheckIgnoreParams(int beforeCallIndex, String beforeMethodName, int afterCallIndex,
+            String afterMethodName)
+    {
+        int beforeCalls = 0;
+        int afterCalls = 0;
 
-		for (MethodCall methodCall : methodCalls)
-		{
-			if (methodCall.name.equals(beforeMethodName))
-			{
-				if (beforeCalls == beforeCallIndex)
-				{
-					return (true);
-				}
+        for (MethodCall methodCall : methodCalls)
+        {
+            if (methodCall.name.equals(beforeMethodName))
+            {
+                if (beforeCalls == beforeCallIndex)
+                {
+                    return (true);
+                }
 
-				beforeCalls++;
-			}
-			else if (methodCall.name.equals(afterMethodName))
-			{
-				if (afterCalls == afterCallIndex)
-				{
-					return (false);
-				}
+                beforeCalls++;
+            }
+            else if (methodCall.name.equals(afterMethodName))
+            {
+                if (afterCalls == afterCallIndex)
+                {
+                    return (false);
+                }
 
-				afterCalls++;
-			}
-		}
+                afterCalls++;
+            }
+        }
 
-		return (false);
-	}
+        return (false);
+    }
 
-	/**
-	 * <p>
-	 * Resets this <code>MockObject<code>.
-	 * </p>
-	 */
-	public void reset()
-	{
-		methodCalls.clear();
-	}
+    /**
+     * <p>
+     * Checks the equality of the two given sets of parameters.
+     * </p>
+     * 
+     * @param parameterSetA
+     *            The first set of parameters to compare.
+     * @param parameterSetB
+     *            The second set of parameters to compare.
+     * 
+     * @return True if the sets of parameters are equal, false otherwise.
+     */
+    protected boolean parametersEqual(Object[] parameterSetA, Object[] parameterSetB)
+    {        
+        if (parameterSetA == null && parameterSetB == null)
+        {
+            return (true);
+        }
+
+        if (parameterSetA == null || parameterSetB == null)
+        {
+            return (false);
+        }
+
+        if (parameterSetA.length != parameterSetB.length)
+        {
+            return (false);
+        }
+
+        for (int index = 0; index < parameterSetA.length; index++)
+        {
+            if (parameterSetA[index].getClass().isEnum() || parameterSetB[index].getClass().isEnum())
+            {                
+                try
+                {
+                if (((Enum) parameterSetA[index]) != ((Enum) parameterSetB[index]))
+                {
+                    return (false);
+                }
+                }
+                catch (ClassCastException e)
+                {}
+            }
+            else if (!parameterSetA[index].equals(parameterSetB[index]))
+            {
+                return (false);
+            }
+        }
+
+        return (true);
+    }
+
+    /**
+     * <p>
+     * Resets this <code>MockObject<code>.
+     * </p>
+     */
+    public void reset()
+    {
+        methodCalls.clear();
+    }
 }
