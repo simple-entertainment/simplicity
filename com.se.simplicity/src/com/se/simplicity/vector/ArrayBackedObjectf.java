cb5c5feca88e0c47e30ab4dcf4c9826321570d57
Gary Buyn <simplegsb@gmail.com>
2010-08-18 16:36:12 +0000

    Implemented the equals method to compare the contents of the object's arrays.

--- com.se.simplicity/src/com/se/simplicity/vector/ArrayBackedObjectf.java
+++ com.se.simplicity/src/com/se/simplicity/vector/ArrayBackedObjectf.java
@@ -6,17 +6,17 @@ import java.io.Serializable;
  * <p>
  * An object which stores its data in a <code>float</code> array.
  * </p>
- *
+ * 
  * @author simple
  */
 public abstract class ArrayBackedObjectf implements Serializable
 {
     /**
-	 * The version of this class.
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	/**
+     * The version of this class.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
      * <p>
      * The array that contains the data for this object.
      * </p>
@@ -25,9 +25,46 @@ public abstract class ArrayBackedObjectf implements Serializable
 
     /**
      * <p>
+     * Returns true if the argument is not null, is of the type <code>ArrayBackedObjectf</code> and has an array the same size with the same contents.
+     * </p>
+     * 
+     * @param 
+     */
+    public boolean equals(Object other)
+    {
+        if (other == null)
+        {
+            return (false);
+        }
+        
+        if (!(other instanceof ArrayBackedObjectf))
+        {
+            return (false);
+        }
+        
+        ArrayBackedObjectf otherArrayBackedObjectf = (ArrayBackedObjectf) other;
+        
+        if (array.length != otherArrayBackedObjectf.getArray().length)
+        {
+            return (false);
+        }
+        
+        for (int index = 0; index < array.length; index++)
+        {
+            if (array[index] != otherArrayBackedObjectf.getArray()[index])
+            {
+                return (false);
+            }
+        }
+        
+        return (true);
+    }
+
+    /**
+     * <p>
      * Retrieves the array that contains the data for this object.
      * </p>
-     *
+     * 
      * @return The array that contains the data for this object.
      */
     public float[] getArray()
@@ -39,7 +76,7 @@ public abstract class ArrayBackedObjectf implements Serializable
      * <p>
      * Retrieves a copy of the array that contains the data for this object.
      * </p>
-     *
+     * 
      * @return A copy of the array that contains the data for this object.
      */
     public float[] getArrayCopy()
@@ -54,7 +91,7 @@ public abstract class ArrayBackedObjectf implements Serializable
      * <p>
      * Sets the array that contains the data for this object.
      * </p>
-     *
+     * 
      * @param array The array that contains the data for this object.
      */
     public void setArray(final float[] array)
