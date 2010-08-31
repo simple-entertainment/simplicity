78dbf57fc673067e6794c357c68013f3d95ce8ba
Gary Buyn <simplegsb@gmail.com>
2010-08-31 17:51:51 +0000

    Added scroll listening to zoom in and out.

--- com.se.simplicity.editor.test/src/com/se/simplicity/editor/test/ui/editors/VisualSceneMouseListenerTest.java
+++ com.se.simplicity.editor.test/src/com/se/simplicity/editor/test/ui/editors/VisualSceneMouseListenerTest.java
@@ -15,6 +15,7 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
 import java.awt.Dimension;
 
@@ -26,6 +27,10 @@ import org.junit.Test;
 
 import com.se.simplicity.editor.ui.editors.VisualSceneMouseListener;
 import com.se.simplicity.picking.engine.PickingEngine;
+import com.se.simplicity.rendering.Camera;
+import com.se.simplicity.scenegraph.Node;
+import com.se.simplicity.vector.SimpleTransformationMatrixf44;
+import com.se.simplicity.vector.TranslationVectorf;
 
 /**
  * <p>
@@ -71,12 +76,62 @@ public class VisualSceneMouseListenerTest
         replay(mockCanvas, mockPickingEngine);
 
         // Initialise test environment.
-        testObject = new VisualSceneMouseListener(mockPickingEngine);
+        testObject = new VisualSceneMouseListener(mockPickingEngine, null);
 
         // Perform test.
         testObject.mouseUp(mouseEvent);
 
         // Verify test results.
         verify(mockPickingEngine);
+    }
+
+    /**
+     * <p>
+     * Unit test the method {@link com.se.simplicity.editor.ui.editors.VisualSceneMouseListener#mouseUp(MouseEvent) mouseUp(MouseEvent)}.
+     * </p>
+     */
+    @Test
+    public void mouseScrolled()
+    {
+        // Create dependencies.
+        Camera mockCamera = createMock(Camera.class);
+        Node mockNode = createMock(Node.class);
+        SimpleTransformationMatrixf44 matrix = new SimpleTransformationMatrixf44();
+
+        Event event = new Event();
+        event.widget = createMock(GLCanvas.class);
+        MouseEvent mouseEvent = new MouseEvent(event);
+        mouseEvent.count = 1;
+
+        // Dictate correct behaviour.
+        expect(mockCamera.getNode()).andStubReturn(mockNode);
+        expect(mockNode.getTransformation()).andStubReturn(matrix);
+        replay(mockCamera, mockNode);
+
+        // Initialise test environment.
+        testObject = new VisualSceneMouseListener(null, mockCamera);
+
+        // Perform test 1.
+        testObject.mouseScrolled(mouseEvent);
+
+        // Verify test 1 results.
+        TranslationVectorf vector = matrix.getTranslation();
+
+        assertEquals(0.0f, vector.getX(), 0.0f);
+        assertEquals(0.0f, vector.getY(), 0.0f);
+        assertEquals(-1.0f, vector.getZ(), 0.0f);
+
+        // Modify dependencies.
+        mouseEvent.count = -1;
+
+        // Perform test 2.
+        testObject.mouseScrolled(mouseEvent);
+
+        // Verify test 2 results.
+        vector = matrix.getTranslation();
+
+        assertEquals(0.0f, vector.getX(), 0.0f);
+        assertEquals(0.0f, vector.getY(), 0.0f);
+        assertEquals(0.0f, vector.getZ(), 0.0f);
     }
 }
