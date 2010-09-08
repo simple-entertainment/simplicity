d8634c6f53f39c6c31d3b981c898b9a691b7267f
Gary Buyn <simplegsb@gmail.com>
2010-09-08 16:22:05 +0000

    Added commands for DrawingMode and editing mode.

---
+++ com.se.simplicity.editor/src/com/se/simplicity/editor/internal/Widget.java
@@ -0,0 +1,32 @@
+package com.se.simplicity.editor.internal;
+
+/**
+ * <p>
+ * The widgets that can be used manipulate the content of a {@link com.se.simplicity.editor.internal.ContentProvider ContentProvider}.
+ * </p>
+ * 
+ * @author Gary Buyn
+ */
+public enum Widget
+{
+    /**
+     * <p>
+     * No widget.
+     * </p>
+     */
+    NONE,
+
+    /**
+     * <p>
+     * The rotation widget.
+     * </p>
+     */
+    ROTATION,
+
+    /**
+     * <p>
+     * The translation widget.
+     * </p>
+     */
+    TRANSLATION
+}
---
+++ com.se.simplicity.editor/src/com/se/simplicity/editor/internal/WidgetPickListener.java
@@ -0,0 +1,60 @@
+/*
+    This file is part of The Simplicity Engine.
+
+    The Simplicity Engine is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    The Simplicity Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with The Simplicity Engine. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.se.simplicity.editor.internal;
+
+import com.se.simplicity.picking.event.PickEvent;
+import com.se.simplicity.picking.event.PickListener;
+import com.se.simplicity.scenegraph.model.ModelNode;
+
+/**
+ * <p>
+ * Listens for pick events on a 3D canvas and sets the picked {@link com.se.simplicity.scenegraph.Node Node} to be the currently selected widget
+ * component.
+ * </p>
+ * 
+ * @author Gary Buyn
+ */
+public class WidgetPickListener implements PickListener
+{
+    /**
+     * <p>
+     * The {@link com.se.simplicity.rendering.editor.internal.ContentProvider ContentProvider} to set the currently selected widget component for.
+     * </p>
+     */
+    private ContentProvider fContentProvider;
+
+    /**
+     * <p>
+     * Creates an instance of <code>WidgetPickListener</code>.
+     * </p>
+     * 
+     * @param contentProvider The {@link com.se.simplicity.rendering.editor.internal.ContentProvider ContentProvider} to set the currently selected
+     * widget component for.
+     */
+    public WidgetPickListener(final ContentProvider contentProvider)
+    {
+        fContentProvider = contentProvider;
+    }
+
+    @Override
+    public void scenePicked(final PickEvent event)
+    {
+        if (event.getHitCount() > 0)
+        {
+            fContentProvider.setSelectedWidgetComponent((ModelNode) event.getCloseHit().getNode());
+        }
+        else
+        {
+            fContentProvider.setSelectedWidgetComponent(null);
+        }
+    }
+}
