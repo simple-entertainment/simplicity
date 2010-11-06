93f0c4796a18e026ee17a7b1be5509a83bd9e86c
Gary Buyn <simplegsb@gmail.com>
2010-11-06 18:04:59 +0000

    Initial import.

---
+++ com.se.devenvy.test/src/com/se/devenvy/test/mocks/MockEnum.java
@@ -0,0 +1,25 @@
+package com.se.devenvy.test.mocks;
+
+/**
+ * <p>
+ * A simple Enum example used only for testing purposes.
+ * </p>
+ * 
+ * @author Gary Buyn
+ */
+public enum MockEnum
+{
+    /**
+     * <p>
+     * First test value.
+     * </p>
+     */
+    TEST0,
+
+    /**
+     * <p>
+     * Second test value.
+     * </p>
+     */
+    TEST1
+}
---
+++ com.se.devenvy.test/src/com/se/devenvy/test/mocks/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>
+ * A test suite for the com.se.devenvy.mocks package.
+ * </p>
+ */
+package com.se.devenvy.test.mocks;
+
