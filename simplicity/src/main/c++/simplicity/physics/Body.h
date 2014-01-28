dc6879390a2e55f685dcb4850a461de0ab9bea0b
Gary Buyn <simplegsb@gmail.com>
2014-01-29 11:00:30 +1300

    Added physics interface and abstract factory.

---
+++ simplicity/src/main/c++/simplicity/physics/Body.h
@@ -0,0 +1,57 @@
+#ifndef BODY_H_
+#define BODY_H_
+
+#include "../Component.h"
+#include "../model/Model.h"
+
+namespace simplicity
+{
+	class Body : public Component
+	{
+		public:
+			struct Material
+			{
+				float density;
+
+				float friction;
+
+				float restitution;
+			};
+
+			virtual ~Body()
+			{
+			}
+
+			//virtual void applyAngularImpulse(float impulse) = 0;
+
+			virtual void applyForce(const Vector3& force, const Vector3& position) = 0;
+
+			//virtual void applyLinearImpulse(const Vector3& impulse, const Vector3& position) = 0;
+
+			//virtual void applyTorque(float torque) = 0;
+
+			virtual void clearForces() = 0;
+
+			virtual const Vector3& getLinearAcceleration() const = 0;
+
+			virtual const Vector3& getLinearVelocity() const = 0;
+
+			virtual float getMass() const = 0;
+
+			virtual const Material& getMaterial() const = 0;
+
+			virtual const Model* getModel() const = 0;
+
+			virtual bool isDynamic() = 0;
+
+			virtual void setDynamic(bool dynamic) = 0;
+
+			virtual void setLinearVelocity(const Vector3& linearVelocity) = 0;
+
+			virtual void setMass(float mass) = 0;
+
+			virtual void setMaterial(const Material& material) = 0;
+	};
+}
+
+#endif /* BODY_H_ */
---
+++ simplicity/src/main/c++/simplicity/physics/PhysicsFactory.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2014 Simple Entertainment Limited
+ *
+ * This file is part of The Simplicity Engine.
+ *
+ * The Simplicity Engine is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * The Simplicity Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Simplicity Engine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "../math/MathConstants.h"
+#include "../math/MathFunctions.h"
+#include "PhysicsFactory.h"
+
+using namespace std;
+
+namespace simplicity
+{
+	unique_ptr<PhysicsFactory> PhysicsFactory::instance = unique_ptr<PhysicsFactory>();
+
+	PhysicsFactory& PhysicsFactory::getInstance()
+	{
+		return *instance;
+	}
+
+	void PhysicsFactory::setInstance(unique_ptr<PhysicsFactory> instance)
+	{
+		PhysicsFactory::instance = move(instance);
+	}
+}
